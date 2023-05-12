5356bc21fd96944d486545e912af60b2516c0324
Kevin Huang <kevwhuang@gmail.com>
2023-05-12 17:09:38 -0500

    Challenges 4, 7, 9

---
+++ js/09-dev-tools.js
@@ -0,0 +1,36 @@
+const dogs = [
+    { name: 'Hugo', age: 8 },
+    { name: 'Snickers', age: 2 },
+];
+
+document.body.innerText += 'Check the console!';
+
+console.clear();
+console.log('Hello.');
+console.log('Hello, I am a %s string!', '💩');
+console.log('%cI am some great text.', 'font-size: 2rem;');
+console.dir(document.body);
+console.table(dogs);
+
+console.info('Crocodiles eat 3-4 people per year.');
+console.warn('Oh noooooo!');
+console.error('Shoot!');
+console.assert(1 === 2, 'That is wrong!');
+
+console.count('Wes');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+
+dogs.forEach(dog => {
+    console.groupCollapsed(`${dog.name}`);
+    console.log(`This is ${dog.name}.`);
+    console.log(`${dog.name} is ${dog.age} years old.`);
+    console.log(`${dog.name} is ${dog.age * 7} dog years old.`);
+    console.groupEnd(`${dog.name}`);
+});
+
+console.time('Fetch data');
+fetch('https://api.github.com/users/wesbos')
+    .then(data => data.json());
+console.timeEnd('Fetch data');
