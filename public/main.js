2602f89471e43287e67898268387f12dbc0852e8
Kevin Huang <kevwhuang@gmail.com>
2023-05-15 13:24:12 -0500

    Final release

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const manifest = [
     '01-drum-kit',
     '02-clock',
-    '03-variables',
+    '03-css-variables',
     '04-array-cardio-1',
     '05-flex-gallery',
     '06-ajax-type-ahead',
@@ -20,10 +20,10 @@ const manifest = [
     '19-webcam',
     '20-speech-recognition',
     '21-geolocation',
-    '22-links',
+    '22-follow-along-links',
     '23-speech-synthesis',
     '24-sticky-nav',
-    '25-events',
+    '25-event-delegation',
     '26-dropdown',
     '27-click-scroll',
     '28-video-speed',
@@ -33,38 +33,50 @@ const manifest = [
 
 let pos;
 
+addEventListener('keyup', e => {
+    switch (e.key) {
+        case 'ArrowUp':
+            back5();
+            break;
+        case 'ArrowLeft':
+            back();
+            break;
+        case 'ArrowRight':
+            forward();
+            break;
+        case 'ArrowDown':
+            forward5();
+            break;
+    }
+});
+
 function back5() {
     typeof pos !== 'number' ? pos = manifest.length - 5 : pos -= 5;
     if (pos < 0) pos += manifest.length;
-
-    document.querySelector('iframe').title = `html/${manifest[pos]}.html`;
-    document.querySelector('iframe').src = `html/${manifest[pos]}.html`;
-    document.querySelector('h1').textContent = `${manifest[pos].replaceAll('-', ' ')}`;
+    iframe();
 }
 
 function back() {
     typeof pos !== 'number' ? pos = manifest.length - 1 : pos -= 1;
     if (pos < 0) pos = manifest.length - 1;
-
-    document.querySelector('iframe').title = `html/${manifest[pos]}.html`;
-    document.querySelector('iframe').src = `html/${manifest[pos]}.html`;
-    document.querySelector('h1').textContent = `${manifest[pos].replaceAll('-', ' ')}`;
+    iframe();
 }
 
 function forward() {
     typeof pos !== 'number' ? pos = 0 : pos += 1;
     if (pos > manifest.length - 1) pos = 0;
-
-    document.querySelector('iframe').title = `html/${manifest[pos]}.html`;
-    document.querySelector('iframe').src = `html/${manifest[pos]}.html`;
-    document.querySelector('h1').textContent = `${manifest[pos].replaceAll('-', ' ')}`;
+    iframe();
 }
 
 function forward5() {
     typeof pos !== 'number' ? pos = 4 : pos += 5;
     if (pos > manifest.length - 1) pos -= manifest.length;
+    iframe();
+}
 
+function iframe() {
     document.querySelector('iframe').title = `html/${manifest[pos]}.html`;
     document.querySelector('iframe').src = `html/${manifest[pos]}.html`;
-    document.querySelector('h1').textContent = `${manifest[pos].replaceAll('-', ' ')}`;
+    document.querySelector('h1').textContent = pos !== 29 ?
+        `${manifest[pos].replaceAll('-', ' ')}` : `${manifest[pos].replace('-', ' ')}`;
 }
